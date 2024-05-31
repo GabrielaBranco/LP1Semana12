8330b68d35e3fdf35bf8e11b191303282fcb4001
Gabriela Branco <lindosogb@gmail.com>
2024-05-31 17:16:17 +0100

    Added Palindrome check that returns if it is a palindrome or not

---
+++ Palindrome/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var arg in args)
+            {
+                bool isPalindrome = CheckPalindrome(arg);
+                Console.WriteLine($"{arg} -> {isPalindrome}");
+            }
+        }
+
+        public static bool CheckPalindrome(string input)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException(nameof(input), "Missing input!");
+            }
+            if (input.Length < 2)
+            {
+                return true;
+            }
+
+            bool IsPalindrome(string word)
+            {
+                int start = 0;
+                int end = word.Length - 1;
+
+                while (start < end)
+                {
+                    if (word[start] != word[end])
+                    {
+                        return false;
+                    }
+                    start++;
+                    end--;
+                }
+                return true;
+            }
+
+            return IsPalindrome(input);
+        }
+    }
+}
